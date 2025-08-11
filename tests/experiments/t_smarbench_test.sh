576104f3646d417f19c3f3c4a0ff116a1643ab31
Manuele Pasini <manuele.pasini@unibo.it>
2025-08-11 08:40:42 +0200

    fix: updated queries

--- tests/experiments/t_smarbench_test.sh
+++ tests/experiments/t_smarbench_test.sh
@@ -5,14 +5,15 @@ INGESTION_ITERATIONS=1
 QUERY_ITERATIONS=20
 WORKERS=(1 2 4 8 10 16 20 32 64)
 
+echo "Downloading datasets"
+./download_datasets.sh "$size"
+echo " ...done"
 
 for size in "${SIZE[@]}"; do
 
-    echo "Downloading datasets for size: $size"
-    ./download_datasets.sh "$size"
-
-    echo "Creating AeonG database with size: $size"
+    echo "Creating AeonG database with size: $size.."
     ./create_database.sh "$size" $INGESTION_ITERATIONS
+    echo " ...done"
 
     for worker in "${WORKERS[@]}"; do
         echo "Running query experiments for size: $size with $worker workers"
