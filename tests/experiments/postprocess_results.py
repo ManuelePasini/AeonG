3e0944d15da58ad67c3178b743b4a7db86772f50
Manuele Pasini <manuele.pasini@unibo.it>
2025-08-28 09:35:50 +0200

    feat:updated postprocess_results

--- tests/experiments/postprocess_results.py
+++ tests/experiments/postprocess_results.py
@@ -10,11 +10,14 @@ query_stats_path = os.path.join(input_path, "query_stats")
 ingestion_stats_path = os.path.join(input_path, "ingestion_stats")
 
 output_path = os.path.join(input_path, "query_evaluation")
-output_file = os.path.join(output_path, "aeong_statistics.csv")
 
-csv_header = (
+query_statistics_output_file = os.path.join(output_path, "aeong_statistics.csv")
+ingestion_statistics_output_file = os.path.join(output_path, "aeong_ingestion_statistics.csv")
+
+query_csv_header = (
     "test_id,model,datasetSize,threads,queryName,queryType,elapsedTime,numEntities,numMachines"
 )
+ingestion_csv_header = ("test_id,model,startTimestamp,endTimestamp,dataset,datasetSize,threads,elapsedTime,storage")
 
 indexes = {"queryName": 0, "datasetSize": 1, "threads": 2}
 
@@ -48,7 +51,8 @@ myUUID = uuid.uuid4()
 # Create output directory if it doesn't exist
 os.makedirs(output_path, exist_ok=True)
 
-statistics_df = create_csv(output_file, csv_header)
+query_statistics_df = create_csv(query_statistics_output_file, query_csv_header)
+ingestion_statisics_df = create_csv(ingestion_statistics_output_file, ingestion_csv_header)
 
 for file in os.listdir(query_stats_path):
 
@@ -68,9 +72,9 @@ for file in os.listdir(query_stats_path):
             if first_line:
                 data = json.loads(first_line)
                 duration = data["duration"]
-                statistics_df = pd.concat(
+                query_statistics_df = pd.concat(
                     [
-                        statistics_df,
+                        query_statistics_df,
                         pd.DataFrame(
                             [
                                 {
@@ -93,5 +97,38 @@ for file in os.listdir(query_stats_path):
             print(f"Skipping file {file} due to JSON decode error: {e}")
             continue
 
+for file in os.listdir(ingestion_stats_path):
+    if file.endswith(".json"):
+        dataset_size = file.split("_")[indexes["datasetSize"]]
+        iteration = file.split("_")[2]
+        threads = 1
+
+        data = load_first_json(os.path.join(ingestion_stats_path, file))
+        duration = data.get("duration", 99999999)
+        storage_consumption = data.get("storage_consumption",99999999)
+
+        "test_id,model,startTimestamp,endTimestamp,dataset,datasetSize,threads,elapsedTime,storage"
+        ingestion_statisics_df_statistics_df = pd.concat(
+            [
+                query_statistics_df,
+                pd.DataFrame(
+                    [
+                        {
+                            "test_id": str(myUUID),
+                            "model": "aeong",
+                            "startTimestamp": data.get("start_time", ""),
+                            "endTimestamp": data.get("end_time", ""),
+                            "dataset": "smartbench",
+                            "datasetSize": dataset_size,
+                            "threads": 1,
+                            "elapsedTime": int(max(float(duration) * 1000, 1)),
+                            "storage": storage_consumption
+                        }
+                    ]
+                ),
+            ],
+            ignore_index=True,
+        )
+
 # Save the updated DataFrame to CSV
-statistics_df.to_csv(output_file, index=False)
+ingestion_statisics_df_statistics_df.to_csv(ingestion_statistics_output_file, index=False)
