a72bdd1079c8b7c603d5a34600942c7e7596813f
Manuele Pasini <manuele.pasini@unibo.it>
2025-08-29 15:38:50 +0200

    feat:updated postprocess_results

--- tests/experiments/download_datasets.sh
+++ tests/experiments/download_datasets.sh
@@ -3,15 +3,20 @@
 set -e
 
 LINK="https://big.csr.unibo.it/downloads/stgraph/aeong/"
+INTERNAL_LINK="https://137.204.74.24/downloads/stgraph/aeong/"
 TARGET_FOLDER="/home/AeonG/tests/datasets/T-mgBench/"
 FILENAME="$1.cypher"
 
 cd "$TARGET_FOLDER" || exit 1
 
-echo "Downloading dataset..."
-if ! wget --no-check-certificate "${LINK}${FILENAME}" -O "${TARGET_FOLDER}${FILENAME}"; then
-    echo "Error downloading dataset, exiting..."
-    exit 1
+echo "Downloading dataset ..."
+# curl -L -o "./${DATASET_SIZE}.tar" "${LINK}${DATASET_SIZE}.tar"
+if ! wget --no-check-certificate --tries=3 "${LINK}${FILENAME}"; then
+    echo "Primary link failed, trying backup..."
+    wget --no-check-certificate --tries=3 "${INTERNAL_LINK}${FILENAME}" || {
+        echo "Error: failed to download from backup too!"
+        exit 1
+    }
 fi
 
 if [[ -f "$FILENAME" ]]; then
