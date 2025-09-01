0ec82fbee37c789dd2e317c303ba101c6bd5fc4d
Manuele Pasini <manuele.pasini@unibo.it>
2025-09-01 10:53:42 +0200

    fix: wrong path in experiments scripts

--- tests/experiments/t_smarbench_test.sh
+++ tests/experiments/t_smarbench_test.sh
@@ -6,21 +6,21 @@ IFS=',' read -r -a WORKERS <<< "$THREAD"
 for size in "${SIZE[@]}"; do
 
     echo "Downloading datasets $size..."
-    ./download_datasets.sh $size
+    ./tests/experiments/download_datasets.sh $size
     echo " ...done"
 
     echo "Creating AeonG database with size: $size.."
-    ./create_database.sh "$size" $INGESTION_ITERATIONS
+    ./tests/experiments/create_database.sh "$size" $INGESTION_ITERATIONS
     echo " ...done"
 
     for worker in "${WORKERS[@]}"; do
         echo "Running query experiments for size: $size with $worker workers"
-        ./query_dataset.sh "$size" "$worker" $QUERY_ITERATIONS
+        ./tests/experiments/query_dataset.sh "$size" "$worker" $QUERY_ITERATIONS
     done
 done
 
 echo "All experiments completed successfully."
 
 echo "Post-processing results..."
-python3 ./postprocess_results.py
+python3 ./tests/experiments/postprocess_results.py
 echo " ...done"
